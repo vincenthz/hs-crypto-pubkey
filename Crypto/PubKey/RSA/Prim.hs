2fb2d90cc09a6b46e563430eb243cfd8655c5b7b
Vincent Hanquez <vincent@snarc.org>
2012-11-29 07:32:17 +0000

    use i2ospOf from crypto-numbers

--- Crypto/PubKey/RSA/Prim.hs
+++ Crypto/PubKey/RSA/Prim.hs
@@ -17,18 +17,18 @@ import qualified Data.ByteString as B
 import Crypto.Types.PubKey.RSA
 import Crypto.PubKey.RSA.Types
 import Crypto.Number.ModArithmetic (exponantiation, inverse)
-import Crypto.Number.Serialize (os2ip, i2osp)
+import Crypto.Number.Serialize (os2ip, i2ospOf)
 
 {- dpSlow computes the decrypted message not using any precomputed cache value.
    only n and d need to valid. -}
 dpSlow :: Integer -> PrivateKey -> ByteString -> Either Error ByteString
-dpSlow _ pk c = i2ospOf (private_size pk) $ expmod (os2ip c) (private_d pk) (private_n pk)
+dpSlow _ pk c = i2osp (private_size pk) $ expmod (os2ip c) (private_d pk) (private_n pk)
 
 {- dpFast computes the decrypted message more efficiently if the
    precomputed private values are available. mod p and mod q are faster
    to compute than mod pq -}
 dpFast :: Integer -> PrivateKey -> ByteString -> Either Error ByteString
-dpFast r pk c = i2ospOf (private_size pk) (multiplication rm1 (m2 + h * (private_q pk)) (private_n pk))
+dpFast r pk c = i2osp (private_size pk) (multiplication rm1 (m2 + h * (private_q pk)) (private_n pk))
     where
         re  = expmod r (public_e $ private_pub pk) (private_n pk)
         rm1 = fromJust $ inverse r (private_n pk)
@@ -38,19 +38,10 @@ dpFast r pk c = i2ospOf (private_size pk) (multiplication rm1 (m2 + h * (private
         h   = ((private_qinv pk) * (m1 - m2)) `mod` (private_p pk)
 
 ep :: PublicKey -> ByteString -> Either Error ByteString
-ep pk m = i2ospOf (public_size pk) $ expmod (os2ip m) (public_e pk) (public_n pk)
+ep pk m = i2osp (public_size pk) $ expmod (os2ip m) (public_e pk) (public_n pk)
 
-{- convert a positive integer into a bytestring of specific size.
-   if the number is too big, this will returns an error, otherwise it will pad
-   the bytestring of 0 -}
-i2ospOf :: Int -> Integer -> Either Error ByteString
-i2ospOf len m 
-    | lenbytes < len  = Right $ B.replicate (len - lenbytes) 0 `B.append` bytes
-    | lenbytes == len = Right bytes
-    | otherwise       = Left KeyInternalError
-    where
-        lenbytes = B.length bytes
-        bytes    = i2osp m
+i2osp :: Int -> Integer -> Either Error ByteString
+i2osp len m = maybe (Left KeyInternalError) Right $ i2ospOf len m
 
 expmod :: Integer -> Integer -> Integer -> Integer
 expmod = exponantiation
