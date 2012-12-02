cf753e6aaf691c108eae793b14a0730233aee651
Vincent Hanquez <vincent@snarc.org>
2012-12-02 06:59:27 +0000

    add PSS to the modules.

--- Crypto/PubKey/RSA/PSS.hs
+++ Crypto/PubKey/RSA/PSS.hs
@@ -1,9 +1,9 @@
 module Crypto.PubKey.RSA.PSS
-    ( sign
+    ( signWithSalt
     , verify
     ) where
 
-import Crypto.Random
+import Crypto.Random.Types
 import Crypto.Types.PubKey.RSA
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as B
@@ -12,6 +12,9 @@ import Crypto.PubKey.RSA.Types
 import Crypto.Number.Serialize (os2ip, i2ospOf)
 import Data.Maybe (fromJust)
 import Data.Bits (xor)
+
+hash = id
+hashLen = 20
 
 signWithSalt :: ByteString -> PrivateKey -> ByteString -> ByteString
 signWithSalt salt pk m = d 1 pk em
@@ -33,18 +36,20 @@ sign rng =
           (salt,rng') = getRandomBytes rng hashLen
 -}
 
+verify :: PublicKey -> Int -> ByteString -> ByteString -> Bool
 verify pk sLen m s
     | public_size pk /= B.length s = False
     | B.last em /= 0xbc            = False
     | not (B.all (== 0) ps0)       = False
     | b1 /= B.singleton 1          = False
     | otherwise                    = h == h'
-        where em        = ep pk (os2ip s)
+        where em        = ep pk s
               maskedDB  = B.take (B.length em - hashLen - 1) em
               h         = B.take hashLen $ B.drop (B.length maskedDB) em
-              dbmask    = mgf1 hash hashLen h
+              dbLen     = public_size pk - hashLen - 1
+              dbmask    = mgf1 hash hashLen h dbLen
               db        = B.pack $ B.zipWith xor maskedDB dbmask
-              (ps0,z)   = B.break (== 1)
+              (ps0,z)   = B.break (== 1) db
               (b1,salt) = B.splitAt 1 z
               mHash     = hash m
               m'        = B.concat [B.replicate 8 0,mHash,salt]
