fdfe314f519d85d22ac6b6b567de72f422d12948
Vincent Hanquez <vincent@snarc.org>
2012-12-16 09:41:53 +0000

    create a PSS Params structure to record all parameters used for signature and verification

--- Crypto/PubKey/RSA/PSS.hs
+++ Crypto/PubKey/RSA/PSS.hs
@@ -1,7 +1,11 @@
 module Crypto.PubKey.RSA.PSS
-    ( signWithSalt
+    ( PSSParams(..)
+    -- * Sign and verify functions
+    , signWithSalt
     , sign
     , verify
+    -- * mask generation algorithms
+    , mgf1
     ) where
 
 import Crypto.Random.API
@@ -10,52 +14,89 @@ import Data.ByteString (ByteString)
 import qualified Data.ByteString as B
 import Crypto.PubKey.RSA.Prim
 import Crypto.PubKey.RSA.Types
+import Crypto.PubKey.HashDescr
 import Crypto.Number.Serialize (i2ospOf)
 import Data.Maybe (fromJust)
 import Data.Bits (xor)
+import Data.Word
 
-hash = id
-hashLen = 20
+-- | Represent a mask generation algorithm
+type MaskGenAlgorithm = HashFunction -- ^ hash function to use
+                     -> ByteString   -- ^ seed
+                     -> Int          -- ^ length to generate
+                     -> ByteString
 
-signWithSalt :: ByteString -> PrivateKey -> ByteString -> ByteString
-signWithSalt salt pk m = dp pk em
-    where mHash    = hash m
+-- | Parameters for PSS signature/verification.
+data PSSParams = PSSParams { pssHash         :: HashFunction     -- ^ Hash function to use
+                           , pssMaskGenAlg   :: MaskGenAlgorithm -- ^ Mask Gen algorithm to use
+                           , pssSaltLength   :: Int              -- ^ Length of salt
+                           , pssTrailerField :: Word8            -- ^ Trailer field, usually 0xbc
+                           }
+
+-- | Sign using the PSS parameters and the salt explicitely passed as parameters.
+--
+-- the function ignore SaltLength from the PSS Parameters
+signWithSalt :: PSSParams  -- ^ PSS Parameters to use
+             -> ByteString -- ^ Salt to use
+             -> PrivateKey -- ^ RSA Private Key
+             -> ByteString -- ^ Message to sign
+             -> ByteString
+signWithSalt params salt pk m = dp pk em
+    where mHash    = (pssHash params) m
           dbLen    = private_size pk - hashLen - 1
           saltLen  = B.length salt
+          hashLen  = B.length (hash B.empty)
 
           m'       = B.concat [B.replicate 8 0,mHash,salt]
           h        = hash m'
           db       = B.concat [B.replicate (dbLen - saltLen - 1) 0,B.singleton 1,salt]
-          dbmask   = mgf1 hash hashLen h dbLen
+          dbmask   = (pssMaskGenAlg params) hash h dbLen
           maskedDB = B.pack $ B.zipWith xor db dbmask
-          em       = B.concat [maskedDB, h, B.singleton 0xbc]
+          em       = B.concat [maskedDB, h, B.singleton (pssTrailerField params)]
+          hash     = pssHash params
 
-sign :: CPRG g => g -> Int -> PrivateKey -> ByteString -> (ByteString, g)
-sign rng saltLen pk m = (signWithSalt salt pk m, rng')
+-- | Sign using the PSS Parameters
+sign :: CPRG g
+     => g               -- ^ random generator to use to generate the salt
+     -> PSSParams       -- ^ PSS Parameters to use
+     -> PrivateKey      -- ^ RSA Private Key
+     -> ByteString      -- ^ Message to sign
+     -> (ByteString, g)
+sign rng params pk m = (signWithSalt params salt pk m, rng')
     where
-          (salt,rng') = genRandomBytes rng saltLen
+          (salt,rng') = genRandomBytes rng (pssSaltLength params)
 
-verify :: PublicKey -> Int -> ByteString -> ByteString -> Bool
-verify pk sLen m s
-    | public_size pk /= B.length s = False
-    | B.last em /= 0xbc            = False
-    | not (B.all (== 0) ps0)       = False
-    | b1 /= B.singleton 1          = False
-    | otherwise                    = h == h'
+-- | Verify a signature using the PSS Parameters
+verify :: PSSParams  -- ^ PSS Parameters to use to verify,
+                     --   this need to be identical to the parameters when signing
+       -> PublicKey  -- ^ RSA Public Key
+       -> ByteString -- ^ Message to verify
+       -> ByteString -- ^ Signature
+       -> Bool
+verify params pk m s
+    | public_size pk /= B.length s        = False
+    | B.last em /= pssTrailerField params = False
+    | not (B.all (== 0) ps0)              = False
+    | b1 /= B.singleton 1                 = False
+    | otherwise                           = h == h'
         where em        = ep pk s
               maskedDB  = B.take (B.length em - hashLen - 1) em
               h         = B.take hashLen $ B.drop (B.length maskedDB) em
               dbLen     = public_size pk - hashLen - 1
-              dbmask    = mgf1 hash hashLen h dbLen
+              dbmask    = (pssMaskGenAlg params) hash h dbLen
               db        = B.pack $ B.zipWith xor maskedDB dbmask
               (ps0,z)   = B.break (== 1) db
               (b1,salt) = B.splitAt 1 z
               mHash     = hash m
               m'        = B.concat [B.replicate 8 0,mHash,salt]
               h'        = hash m'
+              hash      = pssHash params
+              hashLen   = B.length (hash B.empty)
 
 
-mgf1 hash hashLen seed len = loop B.empty 0
+-- | Mask generation algorithm MGF1
+mgf1 :: MaskGenAlgorithm
+mgf1 hash seed len = loop B.empty 0
     where loop t counter
             | B.length t >= len = B.take len t
             | otherwise         = let counterBS = fromJust $ i2ospOf 4 counter
