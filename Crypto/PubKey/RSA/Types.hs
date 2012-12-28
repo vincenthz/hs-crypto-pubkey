b42e0d2a04b6fb0c01cb703189d2932ac1d81cd7
Vincent Hanquez <vincent@snarc.org>
2012-12-28 07:28:47 +0000

    old error.

--- Crypto/PubKey/RSA/Types.hs
+++ Crypto/PubKey/RSA/Types.hs
@@ -14,6 +14,5 @@ data Error =
     | MessageTooLong            -- ^ the message to encrypt is too long (>= private_size - 11)
     | MessageNotRecognized      -- ^ the message decrypted doesn't have a PKCS15 structure (0 2 .. 0 msg)
     | SignatureTooLong          -- ^ the signature generated through the hash is too long to process with this key
-    | KeyInternalError          -- ^ the whole key is probably not valid, since the message is bigger than the key size
     deriving (Show,Eq)
 
