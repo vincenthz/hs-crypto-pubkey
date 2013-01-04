f3213a422a5c5dddf5ba95e89eb47058fb75778e
Vincent Hanquez <vincent@snarc.org>
2013-01-04 07:19:57 +0000

    fix error documentation to not refer to PKCS15, as OAEP/PSS use the same errors

--- Crypto/PubKey/RSA/Types.hs
+++ Crypto/PubKey/RSA/Types.hs
@@ -18,9 +18,9 @@ data Blinder = Blinder !Integer !Integer
 -- | error possible during encryption, decryption or signing.
 data Error =
       MessageSizeIncorrect -- ^ the message to decrypt is not of the correct size (need to be == private_size)
-    | MessageTooLong       -- ^ the message to encrypt is too long (>= private_size - 11)
+    | MessageTooLong       -- ^ the message to encrypt is too long
     | MessageNotRecognized -- ^ the message decrypted doesn't have a PKCS15 structure (0 2 .. 0 msg)
-    | SignatureTooLong     -- ^ the signature generated through the hash is too long to process with this key
+    | SignatureTooLong     -- ^ the message's digest is too long
     | InvalidParameters    -- ^ some parameters lead to breaking assumptions.
     deriving (Show,Eq)
 
